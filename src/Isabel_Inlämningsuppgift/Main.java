7d6d99c3906c0340bebe58f1e182bc383081a36f
isabeldossantos <isabel.ds.pettersson@hotmail.com>
2023-11-16 09:49:30 +0100

    final.

--- src/Isabel_Inlämningsuppgift/Main.java
+++ src/Isabel_Inlämningsuppgift/Main.java
@@ -30,5 +30,7 @@ public class Main {
         System.out.println("___________ Wow a bit extra maybe? Here it comes:___________");
         System.out.println("Your longest word is: " + myLogicProgramme.getTheLongestWord());
         System.out.println("And the amount of words you have written is: " + myLogicProgramme.getTotalAmountOfWords());
+
+        System.out.println("hejsan");
     }
 }
